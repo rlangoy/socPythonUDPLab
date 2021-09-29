f86cca4e61d8517ed63fe96f149ae2c45e431457
Rune Langøy <rune.langoy@gmail.com>
2021-09-29 11:15:51 +0100

    Pyton3 print () fix

--- python_src/SendUdpMessage.py
+++ python_src/SendUdpMessage.py
@@ -4,8 +4,8 @@ import time
 UDP_IP = "127.0.0.1"
 UDP_PORT = 9050
  
-print "UDP target IP:",   UDP_IP
-print "UDP target port:", UDP_PORT
+print ("UDP target IP:",   UDP_IP)
+print ("UDP target port:", UDP_PORT)
 
 counter=0;
  
